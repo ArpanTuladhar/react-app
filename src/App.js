022c129e587d5470eb637423af1ff9340f182e57
Arpan Tuladhar <arpantuladhar98@gmail.com>
2023-11-20 17:32:06 +0900

    Revert React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tweetContent, setTweetContent] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:8080/create_tweet', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `tweetContent=${encodeURIComponent(tweetContent)}`,
+        mode: 'cors',
+      });
+
+      if (!response.ok) {
+        throw new Error('An error occurred while creating the tweet.');
+      }
+
+      const data = await response.text();
+      setMessage(data); // Set success message
+    } catch (error) {
+      console.error('Error:', error);
+      setMessage('An error occurred while creating the tweet.');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Create a Tweet</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="tweetContent">Tweet Content:</label>
+        <br />
+        <textarea
+          id="tweetContent"
+          name="tweetContent"
+          rows="4"
+          cols="50"
+          required
+          value={tweetContent}
+          onChange={(e) => setTweetContent(e.target.value)}
+        ></textarea>
+        <br />
+        <br />
+        <input type="submit" value="Create Tweet" />
+      </form>
+
+      {message && (
+        <div className={message.includes('error') ? 'error-message' : 'success-message'}>
+          {message}
+        </div>
+      )}
     </div>
   );
 }
